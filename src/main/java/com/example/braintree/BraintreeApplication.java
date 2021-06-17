8a45f0bcda576d8f674d60445f0ff1ecd8d2fe34
Charles Kyewalabye <ckyewalabye@nexient.com>
2021-06-17 02:38:01 -0500

    setting up Monolith project

---
+++ src/main/java/com/example/braintree/BraintreeApplication.java
@@ -0,0 +1,14 @@
+package com.example.braintree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraintreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraintreeApplication.class, args);
+		System.out.println("Application is Running");
+	}
+
+}
